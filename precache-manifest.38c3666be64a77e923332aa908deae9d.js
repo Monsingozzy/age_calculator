b0c115fefa2713397ee076b0dbb43fcec25e9afb
Monsing Mohan <mohansinghyoyo88@gmail.com>
2021-11-15 14:54:17 +0530

    Updates

---
+++ precache-manifest.38c3666be64a77e923332aa908deae9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "47d8443a6c0a03c3a917",
+    "url": "/age_calculator/static/css/main.382b5bd6.chunk.css"
+  },
+  {
+    "revision": "47d8443a6c0a03c3a917",
+    "url": "/age_calculator/static/js/main.47d8443a.chunk.js"
+  },
+  {
+    "revision": "f8d4dc7182515ffdc499",
+    "url": "/age_calculator/static/css/1.fb7c356b.chunk.css"
+  },
+  {
+    "revision": "f8d4dc7182515ffdc499",
+    "url": "/age_calculator/static/js/1.f8d4dc71.chunk.js"
+  },
+  {
+    "revision": "2f07aab18485182ea0ef",
+    "url": "/age_calculator/static/js/runtime~main.2f07aab1.js"
+  },
+  {
+    "revision": "9d18eefc27d4cf2c150249385a0e363f",
+    "url": "/age_calculator/static/media/logo.9d18eefc.svg"
+  },
+  {
+    "revision": "b85dd368732eaf9222f31b1cf156baf2",
+    "url": "/age_calculator/index.html"
+  }
+];
