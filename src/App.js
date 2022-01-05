3852cf8ac7e147acdb7883fd22251d8840047e66
Monsingozzy <mohansinghyoyo88@gmail.com>
2022-01-05 10:51:35 +0530

    fg

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
-import DatePicker from './containers/DatePicker';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import "./App.css";
+import DatePicker from "./containers/DatePicker";
 class App extends Component {
   render() {
     return (
-    
       <div className="App">
-        <div class="center">
-      <img src={logo} className="App-logo" alt="logo"/>
-    </div>
-         <DatePicker />
+        <DatePicker />
       </div>
     );
   }
--- src/containers/DatePicker.js
+++ src/containers/DatePicker.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import moment from 'moment';
-import DayPickerInput from 'react-day-picker/DayPickerInput';
-// import 'react-day-picker/lib/style.css';
-import './DatePicker.css';
+import React from "react";
+import moment from "moment";
+import DayPickerInput from "react-day-picker/DayPickerInput";
 
- class DatePicker extends React.Component {
+import "./DatePicker.css";
 
+class DatePicker extends React.Component {
   constructor(props) {
     super(props);
     this.handleDayChange = this.handleDayChange.bind(this);
@@ -20,7 +19,7 @@ import './DatePicker.css';
   render() {
     const { selectedDay } = this.state;
     const startDate = moment(this.state.selectedDay);
-    const timeEnd =moment();
+    const timeEnd = moment();
     const diff = timeEnd.diff(startDate);
     const diffDuration = moment.duration(diff);
     // console.log("Total Duration in millis:", diffDuration.asMilliseconds());
@@ -29,37 +28,41 @@ import './DatePicker.css';
     // console.log("Hours:", diffDuration.hours());
     // console.log("Minutes:", diffDuration.minutes());
     // console.log("Seconds:", diffDuration.seconds());
-    const year =diffDuration.years();
-    const month =diffDuration.months();
+    const year = diffDuration.years();
+    const month = diffDuration.months();
     const days = diffDuration.days();
     const hours = diffDuration.hours();
     const minutes = diffDuration.minutes();
     const seconds = diffDuration.seconds();
     return (
       <div>
-
         {selectedDay && <p>Day: {selectedDay.toLocaleDateString()}</p>}
         {!selectedDay && <h3>Choose a day</h3>}
         <div className="Result">
-
-        <DayPickerInput onDayChange={this.handleDayChange} />
+          <DayPickerInput onDayChange={this.handleDayChange} />
         </div>
-        
+
         <h3 className="Result">Type your birthday in YYYY-MM-DD format</h3>
-        <h1 className="Result">Your Age is  <br></br>  {year} years<br></br> {month} months  <br></br>  {days} days  <br></br>  {hours} hours  <br></br>  {minutes} minutes  <br></br>  {seconds} seconds </h1>
+        <h1 className="Result">
+          Your Age is <br></br> {year} years<br></br> {month} months <br></br>{" "}
+          {days} days <br></br> {hours} hours <br></br> {minutes} minutes{" "}
+          <br></br> {seconds} seconds{" "}
+        </h1>
 
-   <div class="footer Result">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 ">
+        <div class="footer Result">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12 ">
                 <p>
-                    Developed by <a target="_blank" href="https://monsing-se.netlify.app/">MONSING</a>
+                  Developed by{" "}
+                  <a target="_blank" href="https://monsing-se.netlify.app/">
+                    MONSING
+                  </a>
                 </p>
-                
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</div>
       </div>
     );
   }
